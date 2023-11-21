e7e84f15390bb5f3296d1583c7f3cfd586c6c33f
sakaimao <c0a2206905@edu.teu.ac.jp>
2023-11-21 16:40:45 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -9,19 +9,19 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01/fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)] #練習3
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 2, 1.0), pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 6, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0),pg.transform.rotozoom(kk_img, 6, 1.0), pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 2, 1.0)] #練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = -(tmr%1600) #練習6
+        x = tmr%1600 #練習6
         screen.blit(bg_img, [-x, 0]) #練習4
         screen.blit(bg_img, [1600-x, 0])  #練習6
-        screen.blit(kk_imgs[tmr%2],[300, 200]) #練習5
+        screen.blit(kk_imgs[tmr%10],[300, 200]) #練習5
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(90)
 
 
 if __name__ == "__main__":
