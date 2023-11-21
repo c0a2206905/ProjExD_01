fa9ebaed1d6aa3dda3c39b8b3310ef7e79fec23d
sakaimao <c0a2206905@edu.teu.ac.jp>
2023-11-21 17:19:53 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1
+    bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False)]
     kk_img = pg.image.load("ex01/fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 2, 1.0), pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 6, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0),pg.transform.rotozoom(kk_img, 6, 1.0), pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 2, 1.0)] #練習3
@@ -15,9 +16,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600 #練習6
-        screen.blit(bg_img, [-x, 0]) #練習4
-        screen.blit(bg_img, [1600-x, 0])  #練習6
+        x = tmr%3200 #練習6
+        screen.blit(bg_imgs[0], [-x, 0]) #練習4
+        screen.blit(bg_imgs[1], [1600-x, 0])  #練習6
+        screen.blit(bg_imgs[0], [3200-x, 0])
+        screen.blit(bg_imgs[1], [4800-x, 0])
+
         screen.blit(kk_imgs[tmr%10],[300, 200]) #練習5
         pg.display.update()
         tmr += 1        
